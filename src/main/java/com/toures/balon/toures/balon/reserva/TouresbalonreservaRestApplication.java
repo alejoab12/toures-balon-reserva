cde58357ec7a0ff8982662d17461c022f97fdecd
alejoab12 <33188300+alejoab12@users.noreply.github.com>
2020-03-03 15:09:07 -0500

    Add files via upload

---
+++ src/main/java/com/toures/balon/toures/balon/reserva/TouresbalonreservaRestApplication.java
@@ -0,0 +1,11 @@
+package com.toures.balon.toures.balon.reserva;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class TouresbalonreservaRestApplication extends Application {
+}
